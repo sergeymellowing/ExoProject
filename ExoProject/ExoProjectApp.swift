f543f5f3f112b2910ce4460b4302b7f8adcac97d
sergey <sergey@mellowingfactory.com>
2024-04-18 13:20:06 +0900

    Initial Commit

---
+++ ExoProject/ExoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExoProjectApp.swift
+//  ExoProject
+//
+//  Created by 이준녕 on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ExoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
